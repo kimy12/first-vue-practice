abade8e6a71097a7345dd7c6827c2dc7e2416878
kimy12 <shinkim7941@gmail.com>
2023-01-02 23:42:37 +0900

    ES6 리팩터링&Vuex 설치 및 등록

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -47,7 +47,7 @@ export default {
     }
   },
   components : {
-    Modal : Modal
+    Modal
   }
 }
 </script>
---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+
+// Create a new store instance.
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+
+});
+
+
+
+
